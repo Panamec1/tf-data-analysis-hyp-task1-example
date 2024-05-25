36ed1a663df68cc75b7aebb5a1754ce94d37507a
Panamec1 <72736735+Panamec1@users.noreply.github.com>
2024-05-25 23:57:36 +0300

    1

--- solution.py
+++ solution.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import shapiro
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 905420197 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.08
+    t = x_cnt/x_success - y_cnt/y_success
+
+    if t > alpha:
+        return True
+    else:
+        return False
+
